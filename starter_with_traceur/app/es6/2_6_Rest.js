40ded0e2684e2f1fe9786275e08ba5a2b881da1e
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-19 21:29:37 +0200

    Code for submodule 2.6 (Rest Parameters).

---
+++ starter_with_traceur/app/es6/2_6_Rest.js
@@ -0,0 +1,21 @@
+describe("rest parameters", function() {
+
+    it("is like varargs", function() {
+
+        let doWork = function(name, ...numbers) {
+            let result = 0;
+            numbers.forEach(function(n) {
+                result += n;
+            });
+            return result;
+        };
+
+        //// Does not work because of $traceurRuntime.setProperty()
+        //// and $traceurRuntime.toProperty()
+        // let result = doWork("Scott", 1, 2, 3);
+
+        // expect(result).toBe(6);
+
+    });
+
+});
