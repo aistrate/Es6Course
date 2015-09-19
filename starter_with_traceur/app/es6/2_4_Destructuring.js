80affd81d5ed3af538ffcaad0eb043d804193f87
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-19 19:10:36 +0200

    Fix whitespace.

--- starter_with_traceur/app/es6/2_4_Destructuring.js
+++ starter_with_traceur/app/es6/2_4_Destructuring.js
@@ -1,19 +1,19 @@
 describe("destructuring", function() {
 
-	it("can destructure arrays", function() {
+    it("can destructure arrays", function() {
 
-		// let x = 2;
-		// let y = 3;
+        // let x = 2;
+        // let y = 3;
 
-		// [x, y] = [y, x];
+        // [x, y] = [y, x];
 
-		// let [x, y] = [3, 2];
+        // let [x, y] = [3, 2];
 
 		let [, x, y] = [1, 3, 2];
 
-		expect(x).toBe(3);
-		expect(y).toBe(2);
 
-	});
+        expect(x).toBe(3);
+        expect(y).toBe(2);
 
+    });
 });
