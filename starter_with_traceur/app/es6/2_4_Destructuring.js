39da150c74671e337f605c63e427d139240372c8
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-19 20:55:16 +0200

    Code for submodule 2.4 (Destructuring).

--- starter_with_traceur/app/es6/2_4_Destructuring.js
+++ starter_with_traceur/app/es6/2_4_Destructuring.js
@@ -9,11 +9,47 @@ describe("destructuring", function() {
 
         // let [x, y] = [3, 2];
 
-		let [, x, y] = [1, 3, 2];
+        // let [, x, y] = [1, 3, 2];
 
+        let [, x, y, z] = [1, 3, 2];
 
         expect(x).toBe(3);
         expect(y).toBe(2);
+        expect(z).toBeUndefined();
 
     });
+
+    it("can destructure objects", function() {
+
+        let doWork = function() {
+            return {
+                firstName: "Scott",
+                lastName: "Allen",
+                twitter: "OdeToCode"
+            };
+        };
+
+        //// Not working
+        // let { firstName: firstName, twitter: twitter } = doWork();
+
+        // expect(firstName).toBe("Scott");
+
+    });
+
+    it("works with parameters", function() {
+
+        let doWork = function(url, {data, cache}) {
+            return data;
+        };
+
+        let result = doWork(
+                "api/test", {
+                    data: "test",
+                    cache: false
+                });
+
+        expect(result).toBe("test");
+
+    });
+
 });
