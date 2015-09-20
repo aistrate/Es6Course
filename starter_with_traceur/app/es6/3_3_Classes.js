5c2276eb31e4be36699e225e50139c2d8b803a3c
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-20 19:15:23 +0200

    Code for submodule 3.7 (super).

--- starter_with_traceur/app/es6/3_3_Classes.js
+++ starter_with_traceur/app/es6/3_3_Classes.js
@@ -117,4 +117,49 @@ describe("the class keyword", function() {
         expect(e1.doWork()).toBe("Christopher is working");
 
     });
+
+    it("can invoke super methods", function() {
+
+        class Person {
+
+            constructor(name) {
+                this.name = name;
+            }
+
+            get name() {
+                return this._name;
+            }
+
+            set name(newValue) {
+                if (newValue) {
+                    this._name = newValue;
+                }
+            }
+
+        }
+
+        class Employee extends Person {
+
+            constructor(title, name) {
+                super(name);
+                this._title = title;
+            }
+
+            get title() {
+                return this._title;
+            }
+
+            doWork() {
+                return `${this._name} is working`;
+            }
+
+        }
+
+        let e1 = new Employee("Developer", "Scott");
+
+        expect(e1.name).toBe("Scott");
+        expect(e1.title).toBe("Developer");
+
+    });
+
 });
