ac08f840f71eccde1f3bfd726990312059bf1742
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-20 18:20:19 +0200

    Code for submodule 3.5 (get and set).

--- starter_with_traceur/app/es6/3_3_Classes.js
+++ starter_with_traceur/app/es6/3_3_Classes.js
@@ -48,4 +48,38 @@ describe("the class keyword", function() {
 
     });
 
+
+    it("can have getters and setters", function() {
+
+        class Employee {
+
+            constructor(name) {
+                this.name = name;
+            }
+
+            doWork() {
+                return "complete!";
+            }
+
+            get name() {
+                return this._name.toUpperCase();
+            }
+
+            set name(newValue) {
+                this._name = newValue;
+            }
+
+        }
+
+        let e1 = new Employee("Scott");
+        let e2 = new Employee("Alex");
+
+        expect(e1.name).toBe("SCOTT");
+        expect(e2.name).toBe("ALEX");
+
+        e1.name = "Chris";
+        expect(e1.name).toBe("CHRIS");
+
+    });
+
 });
