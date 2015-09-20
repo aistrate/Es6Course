c08e4eab6b382df8de427974324b21e7136f1c19
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-20 19:41:42 +0200

    Code for submodule 3.8 (Overrides).

--- starter_with_traceur/app/es6/3_3_Classes.js
+++ starter_with_traceur/app/es6/3_3_Classes.js
@@ -162,4 +162,74 @@ describe("the class keyword", function() {
 
     });
 
+    it("can override methods", function() {
+
+        class Person {
+
+            constructor(name) {
+                this.name = name;
+            }
+
+            get name() {
+                return this._name;
+            }
+
+            set name(newValue) {
+                if (newValue) {
+                    this._name = newValue;
+                }
+            }
+
+            doWork() {
+                return "free";
+            }
+
+            toString() {
+                return this.name;
+            }
+
+        }
+
+        class Employee extends Person {
+
+            constructor(title, name) {
+                super(name);
+                this._title = title;
+            }
+
+            get title() {
+                return this._title;
+            }
+
+            doWork() {
+                //return super.doWork() + "paid";
+                return "paid";
+            }
+
+        }
+
+        let p1 = new Person("Alex");
+        let e1 = new Employee("Developer", "Scott");
+
+        expect(p1.doWork()).toBe("free");
+        expect(e1.doWork()).toBe("paid");
+
+        expect(p1.toString()).toBe("Alex");
+        expect(e1.toString()).toBe("Scott");
+
+        let makeEveryoneWork = function(...people) {
+            var results = [];
+            for (var i = 0; i < people.length; i++) {
+                //if (people[i].doWork) {
+                if (people[i] instanceof Person) {
+                    results.push(people[i].doWork());
+                }
+            };
+            return results;
+        };
+
+        expect(makeEveryoneWork(p1, e1, {})).toEqual(["free", "paid"]);
+
+    });
+
 });
