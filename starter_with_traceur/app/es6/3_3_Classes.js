668c14a55bc52a01002afa5df4f45b24587f47b2
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-20 17:58:02 +0200

    Code for submodule 3.4 (constructor).

--- starter_with_traceur/app/es6/3_3_Classes.js
+++ starter_with_traceur/app/es6/3_3_Classes.js
@@ -22,4 +22,30 @@ describe("the class keyword", function() {
 
     });
 
+    it("can have a constructor", function() {
+
+        class Employee {
+
+            constructor(name) {
+                this._name = name;
+            }
+
+            doWork() {
+                return "complete!";
+            }
+
+            getName() {
+                return this._name;
+            }
+
+        }
+
+        let e1 = new Employee("Scott");
+        let e2 = new Employee("Alex");
+
+        expect(e1.getName()).toBe("Scott");
+        expect(e2.getName()).toBe("Alex");
+
+    });
+
 });
