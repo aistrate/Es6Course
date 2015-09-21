3f185a8c21843d2dfe1e48c499a974f53cc0acba
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-21 17:30:20 +0200

    Code for submodule 4.6 (Build Your Own Iterable).

--- starter_with_traceur/app/es6/4_4_Iterators.js
+++ starter_with_traceur/app/es6/4_4_Iterators.js
@@ -51,3 +51,51 @@ describe("for of", function() {
     });
 
 });
+
+describe("iterable", function() {
+
+    it("can be built by implementing Symbol.iterator", function() {
+
+        class Company {
+            constructor() {
+                this.employees = [];
+            }
+
+            addEmployees(...names) {
+                this.employees = this.employees.concat(names);
+            }
+
+            [Symbol.iterator]() {
+                return new ArrayIterator(this.employees);
+            }
+        }
+
+        class ArrayIterator {
+            constructor(array) {
+                this.array = array;
+                this.index = 0;
+            }
+            next() {
+                var result = { value: undefined, done: true };
+                if (this.index < this.array.length) {
+                    result.value = this.array[this.index];
+                    result.done = false;
+                    this.index += 1;
+                }
+                return result;
+            }
+        }
+
+        let count = 0;
+        let company = new Company();
+        company.addEmployees("Tim", "Sue", "Joy", "Tom");
+
+        for (let employee of company) {
+            count += 1;
+        }
+
+        expect(count).toBe(4);
+
+    });
+
+});
