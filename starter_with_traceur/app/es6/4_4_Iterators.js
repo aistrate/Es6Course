0f09bf6f9dd02ad9ce5ed18a4d693a2f4f3b3407
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-21 16:50:20 +0200

    Code for submodule 4.5 (for of).

--- starter_with_traceur/app/es6/4_4_Iterators.js
+++ starter_with_traceur/app/es6/4_4_Iterators.js
@@ -22,11 +22,28 @@ describe("iterables", function() {
         // iterator
         sum = 0;
 
-        let iterator = numbers.values(),
-            next = iterator.next();
+        // let iterator = numbers.values();
+        let iterator = numbers[Symbol.iterator]();
+        let next = iterator.next();
         while (!next.done) {
             sum += next.value;
             next = iterator.next();
+        }
+
+        expect(sum).toBe(10);
+
+    });
+});
+
+describe("for of", function() {
+
+    it("can work with iterators at a high level", function() {
+
+        let sum = 0;
+        let numbers = [1, 2, 3, 4];
+
+        for (let n of numbers) {
+            sum += n;
         }
 
         expect(sum).toBe(10);
