d7914c70f7bc565fcf0bfa9bb0e78b6ba618e05f
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-23 15:27:29 +0200

    Code for submodule 4.9 (Calling Next).

--- starter_with_traceur/app/es6/4_7_Generators.js
+++ starter_with_traceur/app/es6/4_7_Generators.js
@@ -93,4 +93,45 @@ describe("generators", function() {
 
     });
 
+    it("can take a parameter from next(param)", function() {
+
+        let range = function*(start, end) {
+            let current = start;
+            while (current <= end) {
+                let delta = yield current;
+                current += delta || 1;
+            }
+        };
+
+        let range2 = function(start, end) {
+            let current = start;
+            let first = true;
+            return {
+                next(delta = 1) {
+                    let result = { value: undefined, done: true };
+                    if (!first) {
+                        current += delta;
+                    }
+                    if (current <= end) {
+                        result.value = current;
+                        result.done = false;
+                    }
+                    first = false;
+                    return result;
+                }
+            };
+        };
+
+        let result = [];
+        let iterator = range2(1, 10);
+        let next = iterator.next();
+        while (!next.done) {
+            result.push(next.value);
+            next = iterator.next(2);
+        }
+
+        expect(result).toEqual([1, 3, 5, 7, 9]);
+
+    });
+
 });
