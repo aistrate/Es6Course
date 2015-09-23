51d588de7f79173fd66f1a3df4d04f8131e99023
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-23 15:22:14 +0200

    Code for submodule 4.8 (Putting It Together).

--- starter_with_traceur/app/es6/4_7_Generators.js
+++ starter_with_traceur/app/es6/4_7_Generators.js
@@ -12,7 +12,7 @@ describe("generators", function() {
             // }
 
             for (let i = start; i <= end; i++) {
-                console.log(i);
+                // console.log(i);
                 yield i;
             }
         };
@@ -28,13 +28,68 @@ describe("generators", function() {
         // }
 
         let sum = 0;
-        console.log("next");
+        // console.log("next");
         for (let n of numbers(1, 5)) {
             sum += n;
-            console.log("next");
+            // console.log("next");
         }
 
         expect(sum).toBe(15);
+
+    });
+
+    it("can be built by implementing Symbol.iterator", function() {
+
+        class Company {
+            constructor() {
+                this.employees = [];
+            }
+
+            addEmployees(...names) {
+                this.employees = this.employees.concat(names);
+            }
+
+            *[Symbol.iterator]() {
+                for (let e of this.employees) {
+                    console.log(e);
+                    yield e;
+                }
+            }
+        }
+
+        let filter = function*(items, predicate) {
+            for (let item of items) {
+                console.log("filter", item);
+                if (predicate(item)) {
+                    yield item;
+                }
+            }
+        };
+
+        let take = function*(items, number) {
+            let count = 0;
+            if (number < 1) {
+                return;
+            }
+            for (let item of items) {
+                console.log("take", item);
+                yield item;
+                count += 1;
+                if (count >= number) {
+                    return;
+                }
+            }
+        }
+
+        let count = 0;
+        let company = new Company();
+        company.addEmployees("Tim", "Sue", "Joy", "Tom");
+
+        for (let employee of take(filter(company, e => e[0] == 'T'), 1)) {
+            count += 1;
+        }
+
+        expect(count).toBe(1);
 
     });
 
