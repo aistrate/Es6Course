bba4350176c0dbff3e4926d743b76b185a77dfa4
Adrian Istrate <adr_istrate@yahoo.com>
2015-09-28 16:44:39 +0200

    Convert tabs to spaces.

--- starter_with_traceur/app/es6/2_2_let.js
+++ starter_with_traceur/app/es6/2_2_let.js
@@ -1,41 +1,41 @@
 describe("how let works", function() {
 
-	it("will provide block scoping, unlike var", function() {
+    it("will provide block scoping, unlike var", function() {
 
-		var doWork = function(flag) {
+        var doWork = function(flag) {
 
-			// if (flag) {
-			// 	var x = 3;
-			// }
-			// return x;
+            // if (flag) {
+            //     var x = 3;
+            // }
+            // return x;
 
-			if (flag) {
-				let x = 3;
-				return x;
-			}
+            if (flag) {
+                let x = 3;
+                return x;
+            }
 
-		};
+        };
 
-		var result = doWork(true);
-		expect(result).toBe(3);
+        var result = doWork(true);
+        expect(result).toBe(3);
 
-	});
+    });
 
-	it("will provide block scoping with for", function() {
+    it("will provide block scoping with for", function() {
 
-		var doWork = function() {
+        var doWork = function() {
 
-			//for (let i = 0; i < 10; i++) {
-			for (var i = 0; i < 10; i++) {
+            //for (let i = 0; i < 10; i++) {
+            for (var i = 0; i < 10; i++) {
 
-			}
-			return i;
+            }
+            return i;
 
-		};
+        };
 
-		var result = doWork();
-		expect(result).toBe(10);
+        var result = doWork();
+        expect(result).toBe(10);
 
-	});
+    });
 
 });
--- starter_with_traceur/app/es6/2_3_const.js
+++ starter_with_traceur/app/es6/2_3_const.js
@@ -1,34 +1,34 @@
 describe("using const", function() {
 
-	it("will make a variable read-only", function() {
+    it("will make a variable read-only", function() {
 
-		const MAX_SIZE = 10;
+        const MAX_SIZE = 10;
 
-		//MAX_SIZE = 12; // Syntax error
+        //MAX_SIZE = 12; // Syntax error
 
-		expect(MAX_SIZE).toBe(10);
+        expect(MAX_SIZE).toBe(10);
 
-	});
+    });
 
-	it("can shadow outer declaration", function() {
+    it("can shadow outer declaration", function() {
 
-		//let x = 12;
-		const x = 12;
-		var doWork = function() {
-			//const x = 12;
-			//let x = 12;
-			//var x = 12;
+        //let x = 12;
+        const x = 12;
+        var doWork = function() {
+            //const x = 12;
+            //let x = 12;
+            //var x = 12;
 
-			//var x = 10;
-			let x = 10;
+            //var x = 10;
+            let x = 10;
 
-			return x;
-		};
+            return x;
+        };
 
-		var result = doWork();
-		expect(result).toBe(10);
-		expect(x).toBe(12);
+        var result = doWork();
+        expect(result).toBe(10);
+        expect(x).toBe(12);
 
-	});
+    });
 
 });
